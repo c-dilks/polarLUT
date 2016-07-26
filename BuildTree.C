95346257e5481338cd0d71c7515259d7967c45fa
Christopher Dilks <dilks@rcas6008.rcf.bnl.gov>
2016-07-26 16:14:17 -0400

    changed startT to Float_t

--- BuildTree.C
+++ BuildTree.C
@@ -6,7 +6,7 @@ void BuildTree(Int_t debug=0,
   // build tree
   TTree * poltr = new TTree();
   TString treestr =
-    "i/I:fill/I:runnum/I:startT/I:b_pol/F:b_pol_avg/F:y_pol/F:y_pol_avg/F";
+    "i/I:fill/I:runnum/I:startT/F:b_pol/F:b_pol_avg/F:y_pol/F:y_pol_avg/F";
   if(debug) {
     treestr = treestr + 
       ":T/F:midT/F:endT/F:b_p0/F:b_p1/F:y_p0/F:y_p1/F";
