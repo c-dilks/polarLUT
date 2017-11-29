4d00c63fed98460b18cb68bb13723a11bfe67b4d
Christopher Dilks <dilks@rcas6008.rcf.bnl.gov>
2017-11-29 02:23:53 -0500

    merging runs 12 and 13; need to double-check still

--- ComputeOverallPol.C
+++ ComputeOverallPol.C
@@ -2,14 +2,20 @@
 // - integrated luminosity
 // - overall lumi-weighted average polarization +/- overall error, including systematics
 //
+// - MERGING RUNS 12 and 13:
+//   1. hadd run 12 and 13 pol*.root files into one file, but give it year "99"
+//   2. the variable `year` below is used to pick the appropriate systematics etc.;
+//      year==99 is for the 12+13-merged values
+//   3. simply call this script on the hadd-ed file
 //
 //
-// SYSTEMATICS NEED TO BE UPDATED FOR RUN 15!!!!!!!!!!!!!!!!!!!!!!!! (low priority task)
+//
+// SYSTEMATICS NEED TO BE UPDATED FOR RUN 15!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 //
 //
 //
 
-void ComputeOverallPol(TString filename="pol_12_analysed_runs.root") {
+void ComputeOverallPol(TString filename="pol_99_analysed_runs.root") {
   TFile * infile = new TFile(filename.Data(),"READ");
   TTree * tr = (TTree*) infile->Get("poltr");
 
@@ -63,40 +69,51 @@ void ComputeOverallPol(TString filename="pol_12_analysed_runs.root") {
 
 
   // numbers for systematics & correction factors
-  Int_t year;
-  sscanf(filename.Data(),"pol_%d.root",&year);
-  Int_t NFILLS_AN_blue, NFILLS_AN_yell;
+  Int_t year; // run year (use 99 to merge runs 12 and 13)
+  //sscanf(filename.Data(),"pol_%d.root",&year);
+  sscanf(filename.Data(),"pol_%d_analysed_runs.root",&year);
+  Int_t NFILLS_AN_blue, NFILLS_AN_yell, NFILLS_AN_prod;
   Float_t SYS_SCALE_blue;
   Float_t SYS_SCALE_yell;
   Float_t SYS_SCALE_prod;
   switch(year) {
     case 12:
-      NFILLS_AN_blue = 49;
+      NFILLS_AN_blue = 49; // (see table 6 of analysis note)
       NFILLS_AN_yell = 49;
-      SYS_SCALE_blue = 0.034;
+      NFILLS_AN_prod = 49; // use lesser of the two NFILLS_AN_{blue,yell}
+      SYS_SCALE_blue = 0.034; // (see table 5 of analysis note)
       SYS_SCALE_yell = 0.034;
       SYS_SCALE_prod = 0.066;
       break;
     case 13:
-      NFILLS_AN_blue = 138;
+      NFILLS_AN_blue = 138; // (see table 6 of analysis note)
       NFILLS_AN_yell = 139;
-      SYS_SCALE_blue = 0.032;
+      NFILLS_AN_prod = 138; // use lesser of the two NFILLS_AN_{blue,yell}
+      SYS_SCALE_blue = 0.032; // (see table 5 of analysis note)
       SYS_SCALE_yell = 0.033;
       SYS_SCALE_prod = 0.064;
+      break;
+    case 99:
+      NFILLS_AN_blue = 49 + 138; // sum of runs 12 and 13
+      NFILLS_AN_yell = 49 + 139;
+      NFILLS_AN_prod = 49 + 138;
+      SYS_SCALE_blue = 0.034; // use larger uncertainty from runs 12 and 13 (see section 4.3.4)
+      SYS_SCALE_yell = 0.034;
+      SYS_SCALE_prod = 0.066;
       break;
     default:
       fprintf(stderr,"ERROR: unknown year\n");
       return;
   };
-  Float_t SYS_PROFILE_single = 0.022 / TMath::Sqrt(NFILLS);
+  Float_t SYS_PROFILE_single = 0.022 / TMath::Sqrt(NFILLS); // (no need to change this for 12+13-merging)
   Float_t SYS_PROFILE_double = 0.031 / TMath::Sqrt(NFILLS);
 
 
   // overcounting correction
   Double_t SIGMA_POL_CORR_blue = SIGMA_POL_blue * ( NFILLS < NFILLS_AN_blue ? (TMath::Sqrt(1-((Float_t)NFILLS)/((Float_t)NFILLS_AN_blue))) : 0.0 );
   Double_t SIGMA_POL_CORR_yell = SIGMA_POL_yell * ( NFILLS < NFILLS_AN_yell ? (TMath::Sqrt(1-((Float_t)NFILLS)/((Float_t)NFILLS_AN_yell))) : 0.0 );
-  Double_t SIGMA_POL_CORR_prod = SIGMA_POL_prod * ( NFILLS < NFILLS_AN_blue ? (TMath::Sqrt(1-((Float_t)NFILLS)/((Float_t)NFILLS_AN_blue))) : 0.0 )
-                                                * ( NFILLS < NFILLS_AN_yell ? (TMath::Sqrt(1-((Float_t)NFILLS)/((Float_t)NFILLS_AN_yell))) : 0.0 );
+  Double_t SIGMA_POL_CORR_prod = SIGMA_POL_prod * ( NFILLS < NFILLS_AN_prod ? (TMath::Sqrt(1-((Float_t)NFILLS)/((Float_t)NFILLS_AN_prod))) : 0.0 );
+
 
   // quadrature sum with systematics
   Double_t SIGMA_POL_TOT_blue = TMath::Sqrt( TMath::Power(SIGMA_POL_CORR_blue, 2) + 
@@ -126,8 +143,8 @@ void ComputeOverallPol(TString filename="pol_12_analysed_runs.root") {
     NFILLS,NFILLS_AN_blue,SIGMA_POL_blue,SIGMA_POL_CORR_blue);
   printf("& \\sigma_{\\mathbb{P}_Y,\\text{(fill-to-fill scale)}} = Re\\left[\\sqrt{1-%d/%d}\\right]\\cdot \\left[%.4f\\right] = %.4f \\\\\n",
     NFILLS,NFILLS_AN_yell,SIGMA_POL_yell,SIGMA_POL_CORR_yell);
-  printf("& \\sigma_{\\mathbb{P}_B\\mathbb{P}_Y,\\text{(fill-to-fill scale)}} = Re\\left[\\sqrt{1-%d/%d}\\right]\\cdot Re\\left[\\sqrt{1-%d/%d}\\right]\\cdot \\left[%.4f\\right] = %.4f \\\\\\\\\n",
-    NFILLS,NFILLS_AN_blue,NFILLS,NFILLS_AN_yell,SIGMA_POL_prod,SIGMA_POL_CORR_prod);
+  printf("& \\sigma_{\\mathbb{P}_Y,\\text{(fill-to-fill scale)}} = Re\\left[\\sqrt{1-%d/%d}\\right]\\cdot \\left[%.4f\\right] = %.4f \\\\\n",
+    NFILLS,NFILLS_AN_prod,SIGMA_POL_prod,SIGMA_POL_CORR_prod);
 
   printf("& \\mathbb{P}_B\\cdot\\sigma_{scale}(P_B)/P_B = %.4f \\cdot %.4f = %.4f \\\\\n",POL_blue,SYS_SCALE_blue,POL_blue*SYS_SCALE_blue);
   printf("& \\mathbb{P}_Y\\cdot\\sigma_{scale}(P_Y)/P_Y = %.4f \\cdot %.4f = %.4f \\\\\n",POL_yell,SYS_SCALE_yell,POL_yell*SYS_SCALE_yell);
